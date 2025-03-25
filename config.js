0a4f4696870a3afd4993d2a904195818c0467952
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-03-25 15:42:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
 };
