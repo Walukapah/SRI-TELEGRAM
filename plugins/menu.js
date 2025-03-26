191b62545d5d9570423edc80494ecdc9d2816920
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-03-26 16:26:24 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -13,7 +13,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
+//const config = await readEnv();
 let menu = {
 main: '',
 download: '',
