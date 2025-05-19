25dbd34fd005d1d95cc47d7db0c1df86230dbf48
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-05-19 19:58:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,12 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
+    SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
+    VERSION: process.env.VERSION || "1.0.0" // Added version here
 };
