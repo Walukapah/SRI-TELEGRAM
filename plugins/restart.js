dd6b3b994d9730a02414b29e1a60576a08700d0d
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-05-20 05:53:30 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -4,12 +4,14 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
+    react: "🐬",
     desc: "restart the bot",
     category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+if (!isOwner) return;
 const {exec} = require("child_process")
 reply("restarting...")
 await sleep(1500)
