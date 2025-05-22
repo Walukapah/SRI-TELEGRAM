fc7276dac9b6abd878ad13142de9005eeb0ffff5
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-05-22 14:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -310,7 +310,7 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 }});
 //============================================================================ 
 
-})
+});
 }
 app.get("/", (req, res) => {
 res.send("hey, bot started✅");
