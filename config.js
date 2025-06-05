27552beae402c431c372072333971e2e94db089a
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-05 12:13:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ module.exports = {
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    STATUS_SAVE: process.env.STATUS_SAVE || "true",
     MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
     VERSION: process.env.VERSION || "1.0.0" // Added version here
 };
