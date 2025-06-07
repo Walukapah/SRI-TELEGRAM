2b3b25579b069fc2a78524571d1d6c3b92715717
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-07 14:50:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,14 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || ".",
+    BOT_NAME: process.env.BOT_NAME || "SRI-BOT 🇱🇰",
     OWNER_NUMBER: process.env.OWNER_NUMBER || ["94753670175"],
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
     MENU_IMG_URL: process.env.MENU_IMG_URL || "https://files.catbox.moe/zuhmxp.jpg",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     STATUS_SAVE: process.env.STATUS_SAVE || "true",
+    NEWS_LETTER: process.env.NEWS_LETTER || "",
     MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
     VERSION: process.env.VERSION || "1.0.0" // Added version here
 };
