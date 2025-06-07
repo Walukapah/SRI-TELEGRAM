18ab0b34a575068e89ac94c54275ae083f80a7e4
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-07 06:25:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID,
+    PREFIX: process.env.PREFIX || ".",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || ["94753670175"],
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
