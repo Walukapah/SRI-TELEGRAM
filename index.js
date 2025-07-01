b393492850625afcf7d774e8bb9f99487aac862d
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-07-01 14:44:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const P = require('pino');
 // Configurations
 const config = require('./config');
 const prefix = config.PREFIX || '!';
-const TELEGRAM_TOKEN = config.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN";
+const TELEGRAM_TOKEN = config.TELEGRAM_TOKEN || "7355024353:AAFcH-OAF5l5Fj6-igY4jOtqZ7HtZGRrlYQ";
 const PORT = process.env.PORT || 3000;
 
 // Initialize Express app
